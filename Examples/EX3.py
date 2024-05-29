e1c48a16e2580928e123e90c524782d6ef0aa991
pouriahassani <poria19964@gmail.com>
2024-05-29 02:20:44 +0200

    added example config files

--- Examples/EX3.py
+++ Examples/EX3.py
@@ -1,7 +1,9 @@
-from gem5.components.boards.x86_board import X86Board
+from gem5.components.boards.simple_board import SimpleBoard
 from gem5.components.cachehierarchies.ruby.mesi_two_level_cache_hierarchy import (
     MESITwoLevelCacheHierarchy,
 )
+from gem5.components.processors.simple_processor import SimpleProcessor
+
 from gem5.components.memory.single_channel import SingleChannelDDR3_1600
 from gem5.components.processors.cpu_types import CPUTypes
 from gem5.components.processors.simple_switchable_processor import (
@@ -11,7 +13,7 @@ from gem5.isas import ISA
 from gem5.resources.resource import obtain_resource
 from gem5.simulate.exit_event import ExitEvent
 from gem5.simulate.simulator import Simulator
-
+from gem5.resources.resource import CustomResource
 # Here we setup a MESI Two Level Cache Hierarchy.
 cache_hierarchy = MESITwoLevelCacheHierarchy(
     l1d_size="16kB",
@@ -32,20 +34,21 @@ memory = SingleChannelDDR3_1600(size="3GB")
 # from the starting core types to the switch core types. In this simulation
 # we start with Timing cores to simulate the OS boot, then switch to the
 # Out-of-order (O3) cores for the command we wish to run after boot.
-processor = SimpleSwitchableProcessor(
-    starting_core_type=CPUTypes.TIMING,
-    switch_core_type=CPUTypes.O3,
-    isa=ISA.X86,
-    num_cores=2,
+# Setup the processor (single core, no switching)
+processor = SimpleProcessor(
+    cpu_type=CPUTypes.TIMING,
+    isa=ISA.RISCV,
+    num_cores=1
 )
 
 # Here we setup the board. The X86Board allows for Full-System X86 simulations.
-board = X86Board(
-    clk_freq="3GHz",
-    processor=processor,
-    memory=memory,
-    cache_hierarchy=cache_hierarchy,
-)
+# board = SimpleBoard(
+#     clk_freq="3GHz",
+#     processor=processor,
+#     memory=memory,
+#     cache_hierarchy=cache_hierarchy,
+# )
+board = SimpleBoard(clk_freq="1GHz", processor=SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.RISCV,num_cores=1), memory=SingleChannelDDR3_1600(size="512MB"),cache_hierarchy = cache_hierarchy)
 
 # Here we set the Full System workload.
 # The `set_kernel_disk_workload` function for the X86Board takes a kernel, a
@@ -57,27 +60,30 @@ board = X86Board(
 # then, again, call `m5 exit` to terminate the simulation. After simulation
 # has ended you may inspect `m5out/system.pc.com_1.device` to see the echo
 # output.
-command = (
-    "m5 exit;"
-    + "echo 'This is running on O3 CPU cores.';"
-    + "sleep 1;"
-    + "m5 exit;"
-)
+# command = (
+#     "m5 exit;"
+#     + "echo 'This is running on O3 CPU cores.';"
+#     + "sleep 1;"
+#     + "m5 exit;"
+# )
+# Obtain the binary to run.
+# binary = CustomResource("/workspaces/gem5/tests/test-progs/hello/bin/x86/linux/hello")
+binary = obtain_resource(resource_id="riscv-crf")
+# Set the syscall emulation workload with the binary.
+board.set_se_binary_workload(binary)
 
-board.set_kernel_disk_workload(
-    kernel=obtain_resource("x86-linux-kernel-4.4.186"),
-    disk_image=obtain_resource("x86-ubuntu-18.04-img"),
-    readfile_contents=command,
-)
+simulator = Simulator(board=board)
 
-# Here we setup the simulator. We pass the board to the simulator and run it
-# but also specify what to do when the simulation exits with the `EXIT` exit
-# event. In this case we call the `processor.switch` function on the first
-# exit event. For the 2nd the default action will be triggered which exists
-# the simulator.
-simulator = Simulator(
-    board=board,
-    on_exit_event={ExitEvent.EXIT: (func() for func in [processor.switch])},
-)
-
+# Runs up to the first `m5 exit` command.
 simulator.run()
+
+# Here we can do things to modify the configuration. In this case we switch
+# the CPU cores from the timing CPU cores to the O3 cores. This reason for this
+# is to simulate the OS boot with the timing CPU cores, which are faster, then
+# switch to the O3 cores to run the command we want to run on the O3 cores.
+# processor.switch()
+
+# # Then to resume the simulation we call `simulator.run()` to continue the
+# # simulation until the second, and last, `m5 exit` command.
+# simulator.run()
+
